b7ecf676ec8592cf1d81a4aa9218ee320f43ff09
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-02-08 17:28:34 -0500

    new question being added

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ list }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{list.map( question => {
+        return <QuestionItem key={question.id} question={question}/>
+      })}</ul>
     </section>
   );
 }
